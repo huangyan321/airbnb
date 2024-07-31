a43d6623325e78aee3fbb5d7d727d84fbd6bfa51
huangyan321 <1604549268@qq.com>
2024-07-31 15:08:06 +0800

    feat: supp

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -48,6 +48,15 @@ const RegisterModal = () => {
     <div className="flex flex-col gap-4 mt-3">
       <hr />
       <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}}></Button>
+      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}}></Button>
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account?</div>
+          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
   );
   return (
